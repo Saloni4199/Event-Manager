95c9e4e9d3553c5629c439b4bffa411535caa9e8
Saloni Maheshwari <saloni.maheshwari@nagarro.com>
2022-10-08 15:03:55 +0530

    Added more events to thumbnail list

--- src/app/event-thumbnail/event-thumbnail.component.ts
+++ src/app/event-thumbnail/event-thumbnail.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-event-thumbnail',
   templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.css',]
 })
 export class EventThumbnailComponent  {
     @Input() eventFromList: any;
