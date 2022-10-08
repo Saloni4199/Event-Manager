2944a23332b5d8f72978a1d168041ac3f20cc56f
Saloni Maheshwari <saloni.maheshwari@nagarro.com>
2022-10-08 16:16:34 +0530

    implemented directives

--- src/app/event-thumbnail/event-thumbnail.component.ts
+++ src/app/event-thumbnail/event-thumbnail.component.ts
@@ -7,5 +7,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class EventThumbnailComponent  {
     @Input() eventFromList: any;
+
+    getStartTimeClass(){
+      const isEarlyStart = this.eventFromList && this.eventFromList.time == '8:00 am';
+      const isLateStart = this.eventFromList && this.eventFromList.time == "10:00 am";
+      if(isEarlyStart)
+      return ['green','bold']
+      else if(isLateStart)
+      return ['red','bold']
+      else
+      return['yellow','bold']
+    }
   }
 
